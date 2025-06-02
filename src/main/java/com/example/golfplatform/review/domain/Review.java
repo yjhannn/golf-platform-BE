23922f4e72a38448b0f7dde7a6aa007e33f0bf65
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-02 17:18:38 +0900

    feat : Review 도메인 class 작성

---
+++ src/main/java/com/example/golfplatform/review/domain/Review.java
@@ -0,0 +1,47 @@
+package com.example.golfplatform.review.domain;
+
+import com.example.golfplatform.golfcourse.domain.GolfCourse;
+import com.example.golfplatform.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Review {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "golf_course_id")
+    private GolfCourse golfCourse;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Lob
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
