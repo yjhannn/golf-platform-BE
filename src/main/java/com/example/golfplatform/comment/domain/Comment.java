42653627478be2969f380c07a86af559b238f183
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-02 17:24:41 +0900

    feat : Comment 도메인 class 작성

---
+++ src/main/java/com/example/golfplatform/comment/domain/Comment.java
@@ -0,0 +1,38 @@
+package com.example.golfplatform.comment.domain;
+
+import com.example.golfplatform.post.domain.Post;
+import com.example.golfplatform.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Lob
+    private String content;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
