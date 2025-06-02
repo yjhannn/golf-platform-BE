907684f6b350c72599c37b0602f97924a8c04fa8
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-02 15:05:21 +0900

    Initial commit : Spring Boot GolfPlatform project setup

---
+++ src/main/java/com/example/golfplatform/GolfplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.golfplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/golfplatform/GolfplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.golfplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
