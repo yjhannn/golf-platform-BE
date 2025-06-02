eb8a0ae46caa86e7b2dcecb895ca4bd118aed9c7
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-02 17:22:06 +0900

    feat : Post 도메인 class 작성

---
+++ src/main/java/com/example/golfplatform/post/domain/Post.java
@@ -0,0 +1,47 @@
+package com.example.golfplatform.post.domain;
+
+import com.example.golfplatform.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum Category {
+        FREE, QUESTION, REVIEW
+    }
+}
