34a70dc79fcb41818ef725510784bb0058f2f1d5
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-04 15:33:51 +0900

    feat : Spring Security로 인한 인증 요구 해제하는 기능 구현

--- src/main/java/com/example/golfplatform/config/SecurityConfig.java
+++ src/main/java/com/example/golfplatform/config/SecurityConfig.java
@@ -1,2 +1,25 @@
-package com.example.golfplatform.config;public class SecurityConfig {
+package com.example.golfplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println("SecurityFilterChain Loaded");
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/api/**").permitAll()
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
 }
