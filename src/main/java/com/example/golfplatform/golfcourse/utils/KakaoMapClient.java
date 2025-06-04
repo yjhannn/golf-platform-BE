0cb2d796484d26010bbb8cdf763c5030646ecd22
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-04 15:37:14 +0900

    feat : Kakao Map API 통한 위치 정보 가져오기 구현

--- src/main/java/com/example/golfplatform/golfcourse/utils/KakaoMapClient.java
+++ src/main/java/com/example/golfplatform/golfcourse/utils/KakaoMapClient.java
@@ -1,2 +1,35 @@
-package com.example.golfplatform.golfcourse.utils;public class KakaoMapClient {
+package com.example.golfplatform.golfcourse.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoMapClient {
+    @Value("${kakao.api.key}")
+    private String apiKey;
+
+    private final WebClient webClient = WebClient.builder()
+        .baseUrl("https://dapi.kakao.com")
+        .build();
+
+    public String searchGolfCourses(double lat, double lng, int radius) {
+        System.out.println("Kakao APIP Key: " + apiKey);
+        return webClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/v2/local/search/keyword.json")
+                .queryParam("query", "골프장")
+                .queryParam("x", lng)
+                .queryParam("y", lat)
+                .queryParam("radius", radius)
+                .queryParam("size", 15)
+                .build())
+            .header("Authorization", "KakaoAK " + apiKey)
+            .retrieve()
+            .bodyToMono(String.class)
+            .block();
+    }
+
 }
