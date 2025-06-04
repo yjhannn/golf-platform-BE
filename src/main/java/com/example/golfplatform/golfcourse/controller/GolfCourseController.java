9becd982c3fcc2e1e57ffae4b8fa4ad76914cfa6
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-04 15:36:32 +0900

    feat : 위치 중심 10Km 이내 골프장 리스트 반환 기능 구현

--- src/main/java/com/example/golfplatform/golfcourse/controller/GolfCourseController.java
+++ src/main/java/com/example/golfplatform/golfcourse/controller/GolfCourseController.java
@@ -1,2 +1,22 @@
-package com.example.golfplatform.golfcourse.controller;public class GolfCourseController {
+package com.example.golfplatform.golfcourse.controller;
+
+import com.example.golfplatform.golfcourse.service.GolfCourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/golf-courses")
+@RequiredArgsConstructor
+public class GolfCourseController {
+    private final GolfCourseService golfCourseService;
+
+    @GetMapping("/nearby")
+    public String getNearbyGolfCourse(@RequestParam double lat, @RequestParam double lng,
+        @RequestParam int radius) {
+        return golfCourseService.findNearbyGolfCourses(lat, lng, radius);
+    }
+
 }
--- src/main/java/com/example/golfplatform/golfcourse/service/GolfCourseService.java
+++ src/main/java/com/example/golfplatform/golfcourse/service/GolfCourseService.java
@@ -1,2 +1,16 @@
-package com.example.golfplatform.golfcourse.service;public class GolfCourseService {
+package com.example.golfplatform.golfcourse.service;
+
+import com.example.golfplatform.golfcourse.utils.KakaoMapClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GolfCourseService {
+    private final KakaoMapClient kakaoMapClient;
+
+    public String findNearbyGolfCourses(double lat, double lng, int radius) {
+        return kakaoMapClient.searchGolfCourses(lat, lng, radius);
+    }
+
 }
