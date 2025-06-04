a6d96acafd0f002c101cad7c1404ca86d78d881f
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-04 15:34:24 +0900

    feat : GolfCourseRepository 생성

--- src/main/java/com/example/golfplatform/golfcourse/repository/GolfCourseRepository.java
+++ src/main/java/com/example/golfplatform/golfcourse/repository/GolfCourseRepository.java
@@ -1,2 +1,10 @@
-package com.example.golfplatform.golfcourse.repository;public interface GolfCourseRepository {
+package com.example.golfplatform.golfcourse.repository;
+
+import com.example.golfplatform.golfcourse.domain.GolfCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GolfCourseRepository extends JpaRepository<GolfCourse, Long> {
+
 }
