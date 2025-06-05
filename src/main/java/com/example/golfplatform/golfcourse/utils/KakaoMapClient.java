a049b3fc3c42adbf4cb7e314da2f7cfc97da909d
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-05 16:27:57 +0900

    refactor : GolfCourseService DTO에 맞게 코드 수정

--- src/main/java/com/example/golfplatform/golfcourse/utils/KakaoMapClient.java
+++ src/main/java/com/example/golfplatform/golfcourse/utils/KakaoMapClient.java
@@ -1,5 +1,8 @@
 package com.example.golfplatform.golfcourse.utils;
 
+import com.example.golfplatform.golfcourse.request.KakaoPositionRequest;
+import com.example.golfplatform.golfcourse.response.KakaoApiResponse;
+import com.example.golfplatform.golfcourse.response.KakaoPositionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -15,19 +18,19 @@ public class KakaoMapClient {
         .baseUrl("https://dapi.kakao.com")
         .build();
 
-    public String searchGolfCourses(double lat, double lng, int radius) {
+    public KakaoApiResponse searchGolfCourses(KakaoPositionRequest request) {
         return webClient.get()
             .uri(uriBuilder -> uriBuilder
                 .path("/v2/local/search/keyword.json")
                 .queryParam("query", "골프장")
-                .queryParam("x", lng)
-                .queryParam("y", lat)
-                .queryParam("radius", radius)
+                .queryParam("x", request.lng())
+                .queryParam("y", request.lat())
+                .queryParam("radius", request.radius())
                 .queryParam("size", 15)
                 .build())
             .header("Authorization", "KakaoAK " + apiKey)
             .retrieve()
-            .bodyToMono(String.class)
+            .bodyToMono(KakaoApiResponse.class)
             .block();
     }
 
