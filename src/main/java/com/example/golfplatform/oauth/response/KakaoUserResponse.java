bf7a0b8e722ca56974020fbd9fa8660fa9c049fe
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-10 17:02:15 +0900

    feat : 카카오 로그인 사용자 정보 응답을 위한 DTO 생성

--- src/main/java/com/example/golfplatform/oauth/response/KakaoUserResponse.java
+++ src/main/java/com/example/golfplatform/oauth/response/KakaoUserResponse.java
@@ -1,2 +1,15 @@
-package com.example.golfplatform.oauth.response;public record KakaoUserResponse() {
+package com.example.golfplatform.oauth.response;
+
+public record KakaoUserResponse(
+    Long id,
+    KakaoAccount kakao_account
+) {
+    public record KakaoAccount(
+        Profile profile
+    ) {
+        public record Profile(
+            String nickname,
+            String profile_image_url
+        ) {}
+    }
 }
