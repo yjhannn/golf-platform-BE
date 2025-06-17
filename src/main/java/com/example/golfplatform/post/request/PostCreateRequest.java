0d77828434d9bbc7ab31273585849a2775bddc70
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-18 00:24:35 +0900

    feat : 게시물 작성 및 수정 요청 DTO 구현

--- src/main/java/com/example/golfplatform/post/request/PostCreateRequest.java
+++ src/main/java/com/example/golfplatform/post/request/PostCreateRequest.java
@@ -1,2 +1,11 @@
-package com.example.golfplatform.post.request;public record PostCreateRequest() {
+package com.example.golfplatform.post.request;
+
+import com.example.golfplatform.post.domain.Post;
+
+public record PostCreateRequest(
+    String title,
+    String content,
+    Post.Category category
+) {
+
 }
--- src/main/java/com/example/golfplatform/post/request/PostUpdateRequest.java
+++ src/main/java/com/example/golfplatform/post/request/PostUpdateRequest.java
@@ -2,7 +2,7 @@ package com.example.golfplatform.post.request;
 
 import com.example.golfplatform.post.domain.Post;
 
-public record PostCreateRequest(
+public record PostUpdateRequest(
     String title,
     String content,
     Post.Category category
