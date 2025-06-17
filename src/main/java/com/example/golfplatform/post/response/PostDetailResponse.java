4eb4bdb198a114eefa3a1052e2613b9f22d2c1b2
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-17 23:31:55 +0900

    feat : 게시물 상세정보용 DTO 구현

--- src/main/java/com/example/golfplatform/post/response/PostDetailResponse.java
+++ src/main/java/com/example/golfplatform/post/response/PostDetailResponse.java
@@ -1,2 +1,25 @@
-package com.example.golfplatform.post.response;public record PostDetailResponse() {
+package com.example.golfplatform.post.response;
+
+import com.example.golfplatform.post.domain.Post;
+import java.time.LocalDateTime;
+
+public record PostDetailResponse(
+    Long id,
+    String title,
+    String content,
+    String authorEmail,
+    Post.Category category,
+    LocalDateTime createdAt
+) {
+    public static PostDetailResponse from(Post post) {
+        return new PostDetailResponse(
+            post.getId(),
+            post.getTitle(),
+            post.getContent(),
+            post.getUser().getEmail(),
+            post.getCategory(),
+            post.getCreatedAt()
+        );
+    }
+
 }
