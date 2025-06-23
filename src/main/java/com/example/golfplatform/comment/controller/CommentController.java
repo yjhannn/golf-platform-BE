8abb7d627dad7501c92ce2333be31d3e1b7f5c90
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-24 01:35:01 +0900

    feat : 댓글 작성 및 삭제 기능 구현

--- src/main/java/com/example/golfplatform/comment/controller/CommentController.java
+++ src/main/java/com/example/golfplatform/comment/controller/CommentController.java
@@ -1,2 +1,34 @@
-package com.example.golfplatform.comment.controller;public class CommentController {
+package com.example.golfplatform.comment.controller;
+
+import com.example.golfplatform.comment.request.CommentRequest;
+import com.example.golfplatform.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts/{postId}/comments")
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    ResponseEntity<Void> createComment(@AuthenticationPrincipal Long userId, @PathVariable Long postId,
+        @RequestBody CommentRequest request) {
+        commentService.createComment(userId, postId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    ResponseEntity<Void> deleteComment(@AuthenticationPrincipal Long userId, @PathVariable Long postId) {
+        commentService.deleteComment(userId, postId);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/example/golfplatform/comment/repository/CommentRepository.java
+++ src/main/java/com/example/golfplatform/comment/repository/CommentRepository.java
@@ -1,2 +1,9 @@
-package com.example.golfplatform.comment.repository;public interface CommentRepository {
+package com.example.golfplatform.comment.repository;
+
+import com.example.golfplatform.comment.domain.Comment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
 }
