381712aad5a3e1a73192eb5c6f3f401f5ddcde4d
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-01 12:01:56 +0900

    feat : 사용자 추가정보(평균 타수) enum 생성

---
+++ src/main/java/com/example/golfplatform/user/domain/AverageScore.java
@@ -0,0 +1,5 @@
+package com.example.golfplatform.user.domain;
+
+public enum AverageScore {
+    UNDER_72, BETWEEN_72_80, BETWEEN_81_90, BETWEEN_91_100, OVER_100
+}
---
+++ src/main/java/com/example/golfplatform/user/domain/PreferredRegion.java
@@ -0,0 +1,2 @@
+package com.example.golfplatform.user.domain;public enum PreferredRegion {
+}
