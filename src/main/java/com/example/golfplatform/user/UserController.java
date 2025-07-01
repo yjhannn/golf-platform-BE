69a4ad2a086c448dec1b74b1e08465576e2e5e09
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-01 15:08:00 +0900

    feat : 추가 정보 입력 Controller 구현

--- src/main/java/com/example/golfplatform/user/UserController.java
+++ src/main/java/com/example/golfplatform/user/UserController.java
@@ -1,2 +1,25 @@
-package com.example.golfplatform.user;public class UserController {
+package com.example.golfplatform.user;
+
+import com.example.golfplatform.user.request.AdditionalInfoRequest;
+import com.example.golfplatform.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private UserService userService;
+    @PostMapping("/profile")
+    public ResponseEntity<Void> updateAdditionUserInfo(@AuthenticationPrincipal Long userId,
+        @RequestBody AdditionalInfoRequest request) {
+        userService.updateProfile(userId, request);
+        return ResponseEntity.ok().build();
+    }
+
 }
