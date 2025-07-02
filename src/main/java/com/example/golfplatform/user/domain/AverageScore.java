16b0d84d4bf2ceaa401ac0e9648ba0d1d6838044
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-02 12:19:27 +0900

    refactor : 한국어로추가정보에 대한 요청 및 응답받을 수 있도록 수정

--- src/main/java/com/example/golfplatform/user/domain/AverageScore.java
+++ src/main/java/com/example/golfplatform/user/domain/AverageScore.java
@@ -1,5 +1,30 @@
 package com.example.golfplatform.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AverageScore {
-    UNDER_72, BETWEEN_72_80, BETWEEN_81_90, BETWEEN_91_100, OVER_100
+    UNDER_72("72타 이하"), BETWEEN_72_80("72~80타"), BETWEEN_81_90("81~90타"), BETWEEN_91_100("91~100타"), OVER_100("100타 이상");
+
+    private final String description;
+
+    AverageScore(String description) {
+        this.description = description;
+    }
+
+    @JsonValue
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonCreator
+    public static AverageScore from(String value) {
+        for (AverageScore score : AverageScore.values()) {
+            if (score.description.equals(value)) {
+                return score;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 평균 타수입니다: " + value);
+    }
+
 }
--- src/main/java/com/example/golfplatform/user/domain/PreferredRegion.java
+++ src/main/java/com/example/golfplatform/user/domain/PreferredRegion.java
@@ -1,8 +1,36 @@
 package com.example.golfplatform.user.domain;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 
 public enum PreferredRegion {
-    GYEONGGI, GANGWON, GYEONGSANG, CHUNGCHEONG, JEOLLA, JEJU, OVERSEAS
+    GYEONGGI("경기도"),
+    GANGWON("강원도"),
+    GYEONGSANG("경상도"),
+    CHUNGCHEONG("충청도"),
+    JEOLLA("전라도"),
+    JEJU("제주도"),
+    OVERSEAS("해외");
+
+    private final String description;
+
+    PreferredRegion(String description) {
+        this.description = description;
+    }
+
+    @JsonValue
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonCreator
+    public static PreferredRegion from(String value) {
+        for (PreferredRegion region : PreferredRegion.values()) {
+            if (region.description.equals(value)) {
+                return region;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 지역입니다: " + value);
+    }
 }
