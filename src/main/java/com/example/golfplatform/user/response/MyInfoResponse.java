8bcb36922db633c5d9f26d262b92a96a37a9521f
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-04 18:04:06 +0900

    feat : 마이페이지 유저정보 응답 DTO 생성

--- src/main/java/com/example/golfplatform/user/response/MyInfoResponse.java
+++ src/main/java/com/example/golfplatform/user/response/MyInfoResponse.java
@@ -1,2 +1,12 @@
-package com.example.golfplatform.user.response;public record MyInfoResponse() {
+package com.example.golfplatform.user.response;
+
+import com.example.golfplatform.user.domain.User;
+
+public record MyInfoResponse(
+    String nickname,
+    String email,
+    String phoneNumber,
+    String preferredRegion,
+    String averageScore
+) {
 }
