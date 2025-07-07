c66c91c3419c223f03e5632e8cefaf35ee96e39c
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-07 20:13:21 +0900

    refactor : String타입으로 들어오는 요청을 Enum 형태로 변환 메서드 추가

--- src/main/java/com/example/golfplatform/user/request/UpdateMyInfoRequest.java
+++ src/main/java/com/example/golfplatform/user/request/UpdateMyInfoRequest.java
@@ -1,9 +1,18 @@
 package com.example.golfplatform.user.request;
 
+import com.example.golfplatform.user.domain.AverageScore;
+import com.example.golfplatform.user.domain.PreferredRegion;
+
 public record UpdateMyInfoRequest(
     String phoneNumber,
     String preferredRegion,
     String averageScore
 ) {
+    public PreferredRegion toPreferredRegion() {
+        return PreferredRegion.from(preferredRegion);
+    }
 
+    public AverageScore toAverageScore() {
+        return AverageScore.from(averageScore);
+    }
 }
