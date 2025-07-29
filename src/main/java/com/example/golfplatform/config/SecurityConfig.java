97ad25ed982285b1b7faa0007c248db5127d8216
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-29 12:10:15 +0900

    refactor : @EnableMethodSecurity 어노테이션 추가를 통해 인증된 사용자 검증 기능 추가

--- src/main/java/com/example/golfplatform/config/SecurityConfig.java
+++ src/main/java/com/example/golfplatform/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import com.example.golfplatform.oauth.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
---
+++ src/main/java/com/example/golfplatform/review/request/ReviewCreateRequest.java
@@ -0,0 +1,2 @@
+package com.example.golfplatform.review.request;public record ReviewCreateRequest() {
+}
