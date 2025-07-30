97ebdd11af04a5e29546a53a8c9cc5fd9e4f373e
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-07-30 12:18:32 +0900

    feat : 평균 평점 기능 구현

---
+++ src/main/java/com/example/golfplatform/golfcourse/response/KakaoMetaResponse.java
@@ -0,0 +1,2 @@
+package com.example.golfplatform.golfcourse.response;public record KakaoMetaResponse() {
+}
--- src/main/java/com/example/golfplatform/review/repository/ReviewRepository.java
+++ src/main/java/com/example/golfplatform/review/repository/ReviewRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     default List<Review> findRecentReview(int size) {
@@ -13,4 +14,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
             findAll(PageRequest.of(0, size, org.springframework.data.domain.Sort.by("createdAt").descending()));
         return page.getContent();
     }
+    @Query("SELECT COALESCE(AVG(r.rating), 0) FROM Review r WHERE r.golfCourse.id = :courseId")
+    double findAverageRatingByCourseId(Long courseId);
 }
